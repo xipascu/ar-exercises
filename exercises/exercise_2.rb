93a96482f43419d322414830440309d7097d6806
Ilinca Pascu <xipascu@gmail.com>
2017-11-08 00:11:28 +0000

    exercise 2 completed

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+# Load the second store from the database and assign it to @store2.
+# Update the first store (@store1) instance in the database. (Change its name or something.)
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store1.update(name: "Hipstereala")
+
+# puts @store1
+puts @store1.name
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,7 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
+# Load the third store (into @store3) as you did the other two before.
+# Using Active Record's destroy method, delete the store from the database.
+# Verify that the store has been deleted by again outputting (putsing) the count (as you did in Exercise 1.)
 # Your code goes here ...
