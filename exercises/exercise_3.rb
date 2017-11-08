80eb64aeea95db661d582407d26f61b5b4824aeb
Ilinca Pascu <xipascu@gmail.com>
2017-11-08 00:16:00 +0000

    Exercise 3 completed.

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -9,3 +9,8 @@ puts "----------"
 # Using Active Record's destroy method, delete the store from the database.
 # Verify that the store has been deleted by again outputting (putsing) the count (as you did in Exercise 1.)
 # Your code goes here ...
+
+@store3 = Store.find(3)
+@store3.destroy
+
+puts Store.count
