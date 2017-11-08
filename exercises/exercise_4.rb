46c6ec13d4934dec5c2459cd7e035e3ac0a9c56a
Ilinca Pascu <xipascu@gmail.com>
2017-11-08 02:38:14 +0000

    Changes in exercises.

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,18 +6,6 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Borrowing and modifying the code from Exercise one, create 3 more stores:
-# Surrey (annual_revenue of 224000, carries women's apparel only)
-# Whistler (annual_revenue of 1900000 carries men's apparel only)
-# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
-# Using the where class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. 
-#Assign the results to a variable @mens_stores.
-
-# Loop through each of these stores and output their name and annual revenue on each line.
-# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
-
-# Your code goes here ...
-
 Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
 Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Poodle", last_name: "Moodle", hourly_rate: 60)
+@store1.employees.create(first_name: "Calypso", last_name: "theGoddess", hourly_rate: 40)
+@store1.employees.create(first_name: "Lady", last_name: "theTramp", hourly_rate: 10)
+
+@store2.employees.create(first_name: "Rafiki", last_name: "Baboon", hourly_rate: 20)
+@store2.employees.create(first_name: "Scooby", last_name: "Doo", hourly_rate: 30)
+@store2.employees.create(first_name: "Mica", last_name: "Ela", hourly_rate: 40)
